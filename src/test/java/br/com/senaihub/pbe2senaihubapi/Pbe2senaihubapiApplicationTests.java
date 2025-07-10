92969775f7fb4d6aa437fc6c7a594ad96d5f5559
William Devidé Komel <williamdevide@gmail.com>
2025-07-10 16:20:29 -0300

    Alterados pastas e arquivos de Teste

--- src/test/java/br/com/senaihub/pbe2senaihubapi/Pbe2senaihubapiApplicationTests.java
+++ src/test/java/br/com/senaihub/pbe2senaihubapi/Pbe2senaihubapiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.projeto1;
+package br.com.senaihub.pbe2senaihubapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Projeto1ApplicationTests {
+class Pbe2senaihubapiApplicationTests {
 
 	@Test
 	void contextLoads() {
